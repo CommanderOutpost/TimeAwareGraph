33ee2a6d5d12e7b7930d4299fbde224037ae7148
CommanderOutpost <109445133+CommanderOutpost@users.noreply.github.com>
2023-05-17 15:14:32 +0100

    Update node.py

--- node.py
+++ node.py
@@ -7,8 +7,8 @@ class Node:
         self.value = value
         self.edges = []
 
-    def add_edge(self, node, start_time, end_time):
-        self.edges.append(Edge(self, node, start_time, end_time))
+    def add_edge(self, node, weight, start_time, end_time, direction=None):
+        self.edges.append(Edge(self, node, weight, start_time, end_time, direction))
 
     def get_edges(self):
         return self.edges
@@ -16,22 +16,20 @@ class Node:
     def get_edges_at_time(self, time: datetime) -> List[Edge]:
         return [edge for edge in self.edges if edge.start_time <= time <= edge.end_time]
 
-    def get_nodes_at_time(self, time: datetime) -> List['Node']:
-        return [node for node in self.nodes if node.get_edges_at_time(time)]
+    def get_nearest_neighbors(self, time: datetime) -> List['Node']:
+        neighbors = [edge.to_node if (edge.direction == 'to' or edge.direction is None) and edge.from_node == self else edge.from_node for edge in self.get_edges_at_time(time) if edge.start_time <= time <= edge.end_time]
+        return neighbors
 
     def remove_edge(self, edge: Edge):
         if edge in self.edges:
-            self.edges.remove(edge)
-
-    def get_nearest_neighbors(self, time: datetime) -> List['Node']:
-        neighbors = [edge.to_node if edge.from_node == self else edge.from_node for edge in self.get_edges_at_time(time)]
-        return neighbors
-
+            if (edge.direction == 'to' and edge.from_node == self) or (edge.direction == 'from' and edge.to_node == self) or edge.direction is None:
+                self.edges.remove(edge)
+                
     def get_degree(self, time: datetime) -> int:
-        return len(self.get_edges_at_time(time))
+        return len(self.get_nearest_neighbors(time))
 
     def __str__(self):
-        return f"Node: {self.value}"
+        return f"{self.value}"
 
     def __repr__(self):
-        return f"Node: {self.value}"
+        return f"{self.value}"
