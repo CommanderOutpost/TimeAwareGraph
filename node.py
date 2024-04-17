eb56eb0694a15ae557f9c3ed548f01a38fef8762
rasheed <thcmofskot@gmail.com>
2024-04-17 17:35:08 +0100

    Streamline the condition in remove_edge for readability and efficiency.

--- node.py
+++ node.py
@@ -20,10 +20,9 @@ def get_nearest_neighbors(self, time: datetime) -> List['Node']:
     neighbors = [edge.to_node if (edge.direction == 'to' or edge.direction is None) and edge.from_node == self else edge.from_node for edge in self.get_edges_at_time(time)]
     return neighbors
 
-    def remove_edge(self, edge: Edge):
-        if edge in self.edges:
-            if (edge.direction == 'to' and edge.from_node == self) or (edge.direction == 'from' and edge.to_node == self) or edge.direction is None:
-                self.edges.remove(edge)
+def remove_edge(self, edge: Edge):
+    if edge in self.edges and ((edge.direction == 'to' and edge.from_node == self) or (edge.direction == 'from' and edge.to_node == self) or edge.direction is None):
+        self.edges.remove(edge)
                 
     def get_degree(self, time: datetime) -> int:
         return len(self.get_nearest_neighbors(time))
